6de0a37646d42a496f77f88f098b738709d6b8e9
Knox <josh.knox0611@gmail.com>
2021-02-20 00:42:27 -0800

    codingchallengeeeeeeeee

---
+++ codingchallenge04.js
@@ -0,0 +1,10 @@
+function findOdd(A) {
+    for (i = 0; i < A.length; i++) {
+        let count = 0;
+
+        for (j = 0; j < A.length; j++) {
+            if (A[i] === A[j]) count++
+        }
+        if (count % 2 !== 0) return A[i];
+    }
+}
