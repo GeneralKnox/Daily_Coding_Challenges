ef900f5267ac00b760ae240b24d31dae7e58a841
Knox <josh.knox0611@gmail.com>
2021-04-14 23:08:28 -0700

    daily coding challenge

---
+++ codingchallenge10.js
@@ -0,0 +1,13 @@
+//I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+ //P.S.Each array includes only integer numbers.Output is a number too.
+
+//sample test describe("Basic tests",function(){
+// Test.assertEquals(arrayPlusArray([1, 2, 3], [4, 5, 6]), 21);
+// Test.assertEquals(arrayPlusArray([-1, -2, -3], [-4, -5, -6]), -21);
+// Test.assertEquals(arrayPlusArray([0, 0, 0], [4, 5, 6]), 15);
+// Test.assertEquals(arrayPlusArray([100, 200, 300], [400, 500, 600]), 2100);
+// })
+
+function arrayPlusArray(arr1, arr2) {
+    return arr1 + arr2; //something went wrong
+}
